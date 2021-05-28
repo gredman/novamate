6a58274e6a0a331b3ad0795efc0efc99c800c58a
Gareth Redman <gareth@gareth.computer>
2021-05-28 17:42:58 +1200

    Tidy up VS Code validation

--- Sources/Novamate/ConvertVSCodeExtension.swift
+++ Sources/Novamate/ConvertVSCodeExtension.swift
@@ -7,36 +7,44 @@ import NovamateKit
 struct ConvertVSCodeExtension: ParsableCommand {
     static let configuration = CommandConfiguration(commandName: "convert-extension")
 
-    @Argument(help: "Path to VS code extension") var path: URL
+    @Argument(help: ArgumentHelp("Path to VS code extension", valueName: "path")) var extensionURL: URL
     @Option(help: "Name of language in extension") var languageName: String?
-
     @Flag(help: "Print debug info to stderr") var debug: Bool = false
 
-    func run() throws {
-        Console.debug = debug
+    private var grammarURL: URL?
 
-        let vsCodeExtension = try VSCodeExtension(url: path)
-        Console.debug("loaded extension \(vsCodeExtension)")
-
-        let grammarURL: URL
+    mutating func validate() throws {
+        let vsCodeExtension = try VSCodeExtension(url: extensionURL)
         if let languageName = languageName {
             guard let grammar = vsCodeExtension.contributes.grammars.first(where: { grammar in
                 grammar.language == languageName
             }) else {
-                throw ConversionError(errorDescription: "no language named \(languageName) in extension \(path.absoluteString)")
+                throw ValidationError("no language named \(languageName) in extension \(extensionURL.standardizedFileURL.path). options are \(vsCodeExtension.formattedGrammarNames)")
             }
-            grammarURL = path.appendingPathComponent(grammar.path)
+            grammarURL = extensionURL.appendingPathComponent(grammar.path)
         } else if vsCodeExtension.contributes.grammars.isEmpty {
-            throw ConversionError(errorDescription: "no languages in \(path.absoluteString)")
+            throw ValidationError("no languages in \(extensionURL.standardizedFileURL.path)")
         } else if vsCodeExtension.contributes.grammars.count > 1 {
-            throw ConversionError(errorDescription: "multiple languages in \(path.absoluteString), please specify which")
+            throw ValidationError("multiple languages in \(extensionURL.standardizedFileURL.path), please specify which. options are \(vsCodeExtension.formattedGrammarNames)")
         } else {
             let grammarPath = vsCodeExtension.contributes.grammars.first!.path
-            grammarURL = path.appendingPathComponent(grammarPath)
+            grammarURL = extensionURL.appendingPathComponent(grammarPath)
         }
+    }
 
+    func run() throws {
+        Console.debug = debug
+
+        let grammarURL = self.grammarURL!
         Console.debug("loading grammar from \(grammarURL)")
         let grammar = try VSCodeGrammar(url: grammarURL)
         Console.debug("loaded grammar \(grammar)")
+    }
+}
+
+private extension VSCodeExtension {
+    var formattedGrammarNames: String {
+        let names = contributes.grammars.map(\.language)
+        return ListFormatter().string(from: names)!
     }
 }
